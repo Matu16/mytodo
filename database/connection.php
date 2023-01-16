90bda64995da0aaf7184616af05a804517008d94
Matu16 <93242535+Matu15@users.noreply.github.com>
2023-01-16 09:24:49 +0200

    Update connection.php

--- database/connection.php
+++ database/connection.php
@@ -2,13 +2,6 @@
 
 class Connection {
     public static function make($config) {
-        
-        // 'host' => 'd107209.mysql.zonevs.eu',
-        // 'db' => 'd107209_mytodo',
-        // 'user' => 'd107209_main',
-        // 'password' => 'saoledloll1234',
-        // 'charset' => 'utf8mb4',
-        // 'options' => []
 
         try {
             return new PDO(
